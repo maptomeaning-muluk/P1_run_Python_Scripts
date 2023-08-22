9d58bc35204cc2f62a6ea931cf1a6225ffddb1ae
maptomeaning-muluk <mulukakshay1@gmai.com>
2023-08-22 16:30:55 +0530

    commit

--- buffer.py
+++ buffer.py
@@ -1,3 +1,7 @@
 import arcpy
 
 arcpy.env.workspace = r"D:\SEM III\Programming_3\P1_run_Python_Scripts\Practical_1_ProProject\Practical_1_ProProject\01_Running_Python_Scripts.gdb"
+
+
+arcpy.analysis.Buffer("Wilson_Schools","Wilson_School_Buffer", "500 meters")
+print("process complete")
