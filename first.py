3f197b8771b9c84af03ff0151405e4b516caec19
maptomeaning-muluk <mulukakshay1@gmai.com>
2023-08-22 14:57:29 +0530

    added

--- first.py
+++ first.py
@@ -1,6 +1,4 @@
-print("Hello Ankita, What is your scene with Jason?")
 
-
-name ="Ankita"
-surname = "Beck"
+name ="Tushar"
+surname = "Parate"
 print("My First name is {} and my surname is {}".format(name,surname))
