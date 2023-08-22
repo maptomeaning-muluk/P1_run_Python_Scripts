942c0f6b36863d1d7ea0db50f7ab865f3421cf17
maptomeaning-muluk <mulukakshay1@gmai.com>
2023-08-22 14:54:30 +0530

    The commited

--- first.py
+++ first.py
@@ -1,6 +1,6 @@
 print("Hello Ankita, What is your scene with Jason?")
 
 
-name ="Akshay"
-surname = "Muluk"
+name ="Ankita"
+surname = "Beck"
 print("My First name is {} and my surname is {}".format(name,surname))
