7deba0d38a8f1d078276401b426893e208a39674
maptomeaning-muluk <mulukakshay1@gmai.com>
2023-08-22 15:25:13 +0530

    hello

--- first.py
+++ first.py
@@ -1,4 +1,20 @@
 
-name ="Radhika"
-surname = "Patil"
+name ="Akshay"
+surname = "Muluk"
 print("My First name is {} and my surname is {}".format(name,surname))
+
+age = 26
+
+print(type(name))
+print(type(age))
+
+result =str(age)
+print(type(result))
+
+# str converts integer to string
+
+# Boolean function
+true_boolean = True
+false_boolean = False
+
+print(type(true_boolean))
