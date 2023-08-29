0050863c37baed08efbf14f9ff99c0ff95ce8a05
maptomeaning-muluk <mulukakshay1@gmail.com>
2023-08-29 14:24:42 +0530

    goal

--- list.py
+++ list.py
@@ -3,3 +3,12 @@ cities =['Pune','Benglore','Mumbai','Chennai','Buda-pest']
 print(cities);
 print(type(cities));
 print(cities[2]);
+
+cities.append("Nagpur"),
+cities.append(True);
+cities.append(False)
+
+print(cities);
+cities.insert(3, "Panaji");
+
+print(cities)
