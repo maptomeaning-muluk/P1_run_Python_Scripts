97a72e7598ed17a36e4be97385a1b27798e2f0db
maptomeaning-muluk <mulukakshay1@gmail.com>
2023-08-29 14:44:40 +0530

    message

--- list.py
+++ list.py
@@ -12,3 +12,14 @@ print(cities);
 cities.insert(3, "Panaji");
 
 print(cities)
+
+cities.pop()
+cities.pop()
+
+print(cities)
+
+for city in cities:
+    print("The current name of City is:")
+    print(city)
+
+print("process completed")
