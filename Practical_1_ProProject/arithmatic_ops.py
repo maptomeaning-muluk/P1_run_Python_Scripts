d4b668e39bdc60bd483e9e9e1ccc2a79509fa6a0
maptomeaning-muluk <mulukakshay1@gmail.com>
2023-08-29 14:06:09 +0530

    add

---
+++ Practical_1_ProProject/arithmatic_ops.py
@@ -0,0 +1,19 @@
+n1 = 10
+n2 = 15
+
+
+#addition
+add = n1 + n2;
+print ("The addition of {} and {} is {}".format(n1,n2,add))
+
+sub = n1 - n2;
+print ("The subtraction of {} and {} is {}".format(n1,n2,sub))
+
+multi = n1 * n2;
+print ("The multiplication of {} and {} is {}".format(n1,n2,multi))
+
+div = n1 / n2;
+print ("The divison of {} and {} is {}".format(n1,n2,div))
+
+mod = n1 % n2
+print ("The modulus of {} and {} is {}".format(n1,n2,mod))
